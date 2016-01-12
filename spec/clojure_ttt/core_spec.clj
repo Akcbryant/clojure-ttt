bf506da7a9d287a1a4a04b3c928ab78da786a596
Kirby Bryant <akcbryant@gmail.com>
2016-01-11 16:38:51 -0800

    Get ready for minimax.

--- spec/clojure_ttt/core_spec.clj
+++ spec/clojure_ttt/core_spec.clj
@@ -2,8 +2,35 @@
   (:require [speclj.core :refer :all]
             [clojure-ttt.core :refer :all]))
 
-(describe "a test"
-  (it "FIXME, I fail."
-    (should= 0 1)))
+(describe "Board Logic"
+  (it "player1 refers to 'X' and player2 refers to 'O'. Empty-board is empty."
+    (should= {} empty-board)
+    (should= {0 "X" 1 "O" 2 "X" 3 "O"} {0 player1, 1 player2, 2 player1, 3 player2}))
+
+  (it "Makes a new board when given a board, move position, and player."
+  	(should= {9 player1} (make-move empty-board 9 player1))
+  	(should= {1 player1 2 player2 9 player2} (make-move {1 player1 2 player2} 9 player2)))
+
+  (it "Check if there is a winner otherwise return nil."
+  	(should= nil (winner {}))
+  	(should= nil (winner {1 player1 2 player2}))
+  	(should= nil (winner {1 player1 2 player2 3 player1 4 player2 5 player2 6 player1 7 player1 8 player1 9 player2}))
+  	(should= player1 (winner {1 player1 2 player2 3 player1 4 player2 5 player1 6 player2 7 player1 8 player2 9 player1}))
+	(should= player2 (winner {1 player2 2 player2 3 player2}))
+	(should= player2 (winner {1 player2 2 player2 3 player1 4 player1 5 player2 6 player2 7 player2 8 player2 9 player1}))
+	;This test shows that if both players have a winning position then this returns the first one to be found in the winner list.
+	(should= player1 (winner {1 player1 2 player1 3 player1 4 player2 5 player2 6 player2 7 player2 8 player2 9 player1}))))
+
+ (describe "Rendering Board"
+ 	;___|___|___
+ 	;___|___|___
+ 	;   |   | 
+ 	(it "Empty board should look empty"
+ 		(should= "___|___|___\n___|___|___\n   |   |   \n" (with-out-str (render-board {}))))
+
+ 	(it "Should fill in the appropriate spaces automatically."
+ 		(should= "_X_|_O_|_X_\n_O_|_X_|_O_\n   |   |   \n" 
+ 			(with-out-str (render-board {1 player1 2 player2 3 player1 4 player2 5 player1 6 player2})))))
+
 
 (run-specs)
--- src/clojure_ttt/core.clj
+++ src/clojure_ttt/core.clj
@@ -1,4 +1,67 @@
 (ns clojure-ttt.core)
 
+(def player1 "X")
+(def player2 "O")
+(def empty-board {})
+(def winning-vectors [[1,2,3] [4,5,6] [7,8,9] [1,5,9] [1,4,7] [2,5,8] [3,6,9] [3,5,7]])
+
+(defn make-move [board position player]
+	(merge board {position player}))
+
+(defn winner-check [board]
+	(for [vector winning-vectors]
+		(if (and (= (board (first vector)) (board (second vector)) (board (last vector))) 
+			(not= (board (first vector)) nil))
+			(board (first vector))
+			nil)))
+
+(defn winner [board]
+	(some #{player1 player2} (winner-check board)))
+
+;Terminal stuff - move to other file later.
+
+;Forward Declarations
+(declare process-initial-input)
+(declare prompt-for-move)
+
+(defn render-board [board]
+	(println (str "_" (get board 1 "_") "_|_" (get board 2 "_") "_|_" (get board 3 "_") "_"))
+	(println (str "_" (get board 4 "_") "_|_" (get board 5 "_") "_|_" (get board 6 "_") "_"))
+	(println (str " " (get board 7 " ") " | " (get board 8 " ") " | " (get board 9 " ") " ")))
+
+(defn process-move [input board]
+	(cond (contains? board input)
+		((println "That space is taken!!!")
+		(prompt-for-move board))
+		(or (> input 9) (< input 1))
+		(prompt-for-move board)
+		:else
+		(println "please get better")
+		))
+
+(defn prompt-for-move [board]
+	(render-board board)
+	(println "Where would you like to move: Pick 1-9?")
+	(flush)
+	(process-move ((read-line) board)))
+	
+(defn get-computer-move [board]
+	(println "get-computer-move"))
+
+(defn prompt-for-initial-input []
+	(println "Welcome to Jurassic Par... Tic Tac Toe!!!")
+	(println "Would you like to be X's or O's???")
+	(flush)
+	(process-initial-input (read-line)))
+
+(defn process-initial-input [input]
+	(cond (or (= input "X") (= input "x"))
+		(prompt-for-move empty-board)
+		  (or (= input "O") (= input "o"))
+		(get-computer-move empty-board)
+		:else
+		((println "This is not the time for GAMES!!!")
+		(prompt-for-initial-input))))
+
 (defn -main []
-	(println "CHILL OUT YOOOOO"))
+	(prompt-for-initial-input))
